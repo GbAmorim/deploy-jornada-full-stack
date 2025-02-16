ab69c4b86687522a3d20ee0b36e181ab55891e66
Gabriel Amorim <gabrielamorimdeceia@gmail.com>
2025-02-16 19:16:12 -0300

    Atualizacao

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -4,7 +4,7 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    "mongodb+srv://fullstackjornada:fke1HK8irAa90rYa@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
